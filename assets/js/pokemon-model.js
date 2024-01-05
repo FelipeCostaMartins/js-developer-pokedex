c9038acbf3985b1f12ca655e64db83cc847041a4
Fê <felipecosta2407@gmail.com>
2024-01-05 14:02:06 -0300

    Github pages
    O fork não estava tendo resposta da api na vizualização do github pages, pra testar fiz o fork de um projeto com o github pages funcionando para utilizar como base do meu projeto ao inves do fork original

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,4 +5,17 @@ class Pokemon {
     type;
     types = [];
     photo;
+     baseExp; //adicionado
+    height; //adicionado
+    weight; //adicionado
+    abilities = []; //adicionado
+    moves = []; //adicionado
+    stats = []; //adicionado
+    description; //adicionado
+    generation; //adicionado
+    eggGroups = []; //adicionado
+    evolutionChain; //adicionado
+    evolutions = []; //adicionado
+    evolutionsSprites = []; //adicionado
+    liked; //adicionado
 }
